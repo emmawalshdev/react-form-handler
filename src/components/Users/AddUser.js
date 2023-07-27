1425329a934b833bf4e87c9ac6dadcc9c7bfddea
Emma Walsh <walshemma33@outlook.com>
2023-07-27 22:00:28 +0100

    Add error state handling for modal

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -9,15 +9,24 @@ const AddUser = (props) => {
     // useState
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
+    const [error, setError] = useState();
 
     const addUserHandler = (event) => {
         event.preventDefault();
 
         // don't save values if not validated
         if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0){
+            setError({
+                title: 'Invalid input',
+                message: 'Please enter a valid name & age (non-empty values)',
+            });
             return;
         }
         if (+enteredAge < 1){ //convert to num
+            setError({
+                title: 'Invalid age',
+                message: 'Please enter a value age (>0).',
+            });
             return;
         }
         setEnteredUsername('');
@@ -39,9 +48,13 @@ const AddUser = (props) => {
         }
     }
 
+    const errorHandler = () => {
+        setError(null);
+    }
+
     return ( 
         <div>
-            <ErrorModal title="helo title" message="message title"/>
+            {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
             <Card className={classes.input}>
                 <form onSubmit={addUserHandler}>
                     <label htmlFor="username">Username</label>

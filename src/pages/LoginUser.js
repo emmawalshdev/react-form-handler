fdb40c9d57068726320f55237cb3e844920419ff
Emma Walsh <walshemma33@outlook.com>
2023-08-06 17:00:12 +0100

    Add useEffect to handle login logout behaviour

--- src/pages/LoginUser.js
+++ src/pages/LoginUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from '../components/Login/Login';
 import Home from '../components/Home/Home';
@@ -8,24 +8,34 @@ const LoginUser = () => {
 
     const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+    useEffect(() => {
+      const loggedInInfo = localStorage.getItem('isLoggedIn');
+
+      if(loggedInInfo === '1'){
+        setIsLoggedIn(true);
+      }
+    }, []); // run only if dependencies change
+
     const loginHandler = (email, password) => {
       // We should of course check email and password
       // But it's just a dummy/ demo anyways
       setIsLoggedIn(true);
+      localStorage.setItem('isLoggedIn', '1');
     };
   
     const logoutHandler = () => {
       setIsLoggedIn(false);
+      localStorage.removeItem('isLoggedIn');
     };
 
     
     return (
         <React.Fragment>
-        <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-        <main>
-          {!isLoggedIn && <Login onLogin={loginHandler} />}
-          {isLoggedIn && <Home onLogout={logoutHandler} />}
-        </main>
+          <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+          <main>
+            {!isLoggedIn && <Login onLogin={loginHandler} />}
+            {isLoggedIn && <Home onLogout={logoutHandler} />}
+          </main>
       </React.Fragment>
     )
 }

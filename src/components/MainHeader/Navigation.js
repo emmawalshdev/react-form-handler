884311c36c62e6f2d672c8037f4b28c25751cd1e
Emma Walsh <walshemma33@outlook.com>
2023-08-25 18:50:52 +0100

    Utilize useContext to handle loggedin status

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import classes from './Navigation.module.css';;
+import classes from './Navigation.module.css';
+import AuthContext from '../../context/auth-context';
 
 const Navigation = (props) => {
   return (
-    <nav className={classes.nav}>
-      <ul>
-        <li>
-        <Link to='/users' >
-          Users
-        </Link>
-        </li>
-        {props.isLoggedIn && (
-          <li>
-            <button onClick={props.onLogout}>Logout</button>
-          </li>
-        )}
-      </ul>
-    </nav>
+    <AuthContext.Consumer>
+      {(ctx) => {
+        return (
+          <nav className={classes.nav}>
+          <ul>
+            <li>
+            <Link to='/users' >
+              Users
+            </Link>
+            </li>
+            {ctx.isLoggedIn && (
+              <li>
+                <button onClick={ctx.onLogout}>Logout</button>
+              </li>
+            )}
+          </ul>
+        </nav>
+        )
+      }}
+    </AuthContext.Consumer>
   );
 };
 
--- src/pages/LoginUser.js
+++ src/pages/LoginUser.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import Login from '../components/Login/Login';
 import Home from '../components/Home/Home';
 import MainHeader from '../components/MainHeader/MainHeader';
+import AuthContext from '../context/auth-context';
 
 const LoginUser = () => {
 
@@ -31,13 +32,21 @@ const LoginUser = () => {
   
     return (
         <React.Fragment>
+          {/* make available to all children */}
+          <AuthContext.Provider
+            // if value changes then update is passed down
+            value={{
+              isLoggedIn: isLoggedIn,
+            }}
+          >
           <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
           <main>
             {!isLoggedIn && <Login onLogin={loginHandler} />}
             {isLoggedIn && <Home onLogout={logoutHandler} />}
           </main>
+          </AuthContext.Provider> 
       </React.Fragment>
-    )
+    );
 }
 
 export default LoginUser;

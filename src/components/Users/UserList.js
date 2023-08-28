926ac0f04b7a095f3c37319c22379e9c2fbbe7b4
Emma Walsh <walshemma33@outlook.com>
2023-08-28 17:30:21 +0100

    Hide userList and addUser components if logged out

--- src/components/Users/UserList.js
+++ src/components/Users/UserList.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { Fragment, useContext } from "react";
 import classes from './UserList.module.css';
 import Card from "../UI/Card";
+import AuthContext from "../../context/auth-context";
+
 
 const UserList = (props) => {
-    console.log(props.users);
-    return <Card className={classes.users}>
-        {props.users.length >0 &&  <ul>
-            {props.users.map((user)=> (
-                <li key={user.key}>
-                    {user.name} ({user.age} years old)
-                </li>
-            ))}
-        </ul>
-        }
-    </Card>
+
+    const ctx = useContext(AuthContext);
+
+    return <Fragment>
+        {ctx.isLoggedIn && (
+            <Card className={classes.users}>
+                {props.users.length >0 &&  <ul>
+                    {props.users.map((user)=> (
+                        <li key={user.key}>
+                            {user.name} ({user.age} years old)
+                        </li>
+                    ))}
+                </ul>
+                }
+            </Card>
+        )}
+    </Fragment>
 }
 
 export default UserList;
